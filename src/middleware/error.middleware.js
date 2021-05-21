905cf5e915029fa9e0da98cd2d58756ba8a50ed1
Saiful B. Khan <saifulbkhan@gmail.com>
2021-05-21 18:57:46 +0530

    [Ftr] Pass HTTP requests to services as is

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -2,11 +2,8 @@ module.exports = async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    if (err.status >= 500) {
-      // eslint-disable-next-line no-console
-      console.log("Error handler:", err);
-    }
-
+    // eslint-disable-next-line no-console
+    console.log("Error handler:", err);
     ctx.status = err.status || 500;
     ctx.body = {
       status: "failed",
--- src/server.js
+++ src/server.js
@@ -1,23 +1,34 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const compress = require("koa-compress");
-const helmet = require("koa-helmet");
 const cors = require("@koa/cors");
+const helmet = require("koa-helmet");
+const proxy = require("koa-proxies");
 
 const errorHandler = require("./middleware/error.middleware");
-const applyApiMiddleware = require("./api");
+
+const apiVersion = "v1";
+const serviceProxyUrls = {
+  caduceus: "http://localhost:8888"
+};
+
+const servicePath = serviceName => `/${apiVersion}/${serviceName}`;
+const serviceProxy = serviceName => {
+  return proxy(servicePath("caduceus"), {
+    target: serviceProxyUrls[serviceName],
+    rewrite: path => path.replace(servicePath(serviceName), ""),
+    changeOrigin: true,
+    logs: true
+  });
+};
 
 const server = new Koa();
-
-// Pass middlewares to our server instance
 server
   .use(errorHandler)
   .use(helmet())
   .use(compress())
   .use(cors())
+  .use(serviceProxy("caduceus"))
   .use(bodyParser());
-
-// Add our API router to the server
-applyApiMiddleware(server);
 
 server.listen(3000);
