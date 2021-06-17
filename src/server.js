fa07ddc1a89526141934e3a54006b0fb77bdf68c
Saiful B. Khan <saifulbkhan@gmail.com>
2021-06-17 22:36:05 +0530

    [Add] Proxy for workspace service

--- src/server.js
+++ src/server.js
@@ -9,12 +9,13 @@ const errorHandler = require("./middleware/error.middleware");
 
 const apiVersion = "v1";
 const serviceProxyUrls = {
-  orchestration: "http://localhost:8888"
+  orchestration: "http://localhost:8888",
+  workspace: "http://localhost:4000"
 };
 
 const servicePath = serviceName => `/${apiVersion}/${serviceName}`;
 const serviceProxy = serviceName => {
-  return proxy(servicePath("orchestration"), {
+  return proxy(servicePath(serviceName), {
     target: serviceProxyUrls[serviceName],
     rewrite: path => path.replace(servicePath(serviceName), ""),
     changeOrigin: true,
@@ -29,6 +30,7 @@ server
   .use(compress())
   .use(cors())
   .use(serviceProxy("orchestration"))
+  .use(serviceProxy("workspace"))
   .use(bodyParser());
 
 server.listen(3000);
